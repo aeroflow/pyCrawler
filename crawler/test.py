8289b4be44ecb8ce3cf81eda0c8c619b93312b08
user <user@user-PC>
2012-08-20 17:59:19 +0800

    sfs

--- crawler/test.py
+++ crawler/test.py
@@ -1,22 +1,13 @@
-import urllib2
-from sgmllib import SGMLParser
- 
-class ListName(SGMLParser):
-    def __init__(self):
-        SGMLParser.__init__(self)
-        self.is_h4 = ""
-        self.name = []
-    def start_h4(self, attrs):
-        self.is_h4 = 1
-    def end_h4(self):
-        self.is_h4 = ""
-    def handle_data(self, text):
-        if self.is_h4 == 1:
-            self.name.append(text)
- 
-content = urllib2.urlopen('http://list.taobao.com/browse/cat-0.htm').read()
-listname = ListName()
-listname.feed(content)
-for item in listname.name:
-    print(item.decode('gbk').encode('utf8'))
-
+SUFFIXES = {1000: ['KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'], 1024: ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB']}
+def approximate_size( size, a_kilobyte_is_1024_bytes=True):
+    if size < 0:
+        raise ValueError('number must be non-negative')
+    multiple = 1024 if a_kilobyte_is_1024_bytes else 1000
+    for suffix in SUFFIXES[multiple]:
+        size /= multiple
+        if size < multiple:
+            return '{0:.1f} {1}'.format(size, suffix)
+    raise ValueError('number too large')
+if __name__ == '__main__':
+    print(approximate_size(1000000000000, False))
+    print(approximate_size(1000000000000))
