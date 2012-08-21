ce8577841ed1d1b27cae2ed67d895af5038635c8
user <user@user-PC>
2012-08-21 20:51:13 +0800

    ver0.1

--- crawler/test.py
+++ crawler/test.py
@@ -1,13 +1,33 @@
-SUFFIXES = {1000: ['KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'], 1024: ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB']}
-def approximate_size( size, a_kilobyte_is_1024_bytes=True):
-    if size < 0:
-        raise ValueError('number must be non-negative')
-    multiple = 1024 if a_kilobyte_is_1024_bytes else 1000
-    for suffix in SUFFIXES[multiple]:
-        size /= multiple
-        if size < multiple:
-            return '{0:.1f} {1}'.format(size, suffix)
-    raise ValueError('number too large')
-if __name__ == '__main__':
-    print(approximate_size(1000000000000, False))
-    print(approximate_size(1000000000000))
+import httplib2
+import re
+from html.parser import HTMLParser
+
+rootURL = 'http://www.dianping.com/search/category/2/0'
+
+urlPattern = re.compile(r'<a href="/shop/\d+"(.*)&nbsp;&nbsp;\d{8}</li>')
+
+def getRestaurant(url,filename):
+    print(url)
+    print(filename)
+    try:
+        response, content = httplib2.Http().request(url)
+    except:
+        print('download exception:' + response)
+        return 0
+    f = open(filename, 'w')
+'''    
+Parse the Html and get the information about restaurant
+
+   
+'''
+    
+def getURL(url):
+    try:
+        response, content = httplib2.Http().request(url)
+    except:
+        print('download exception:' + response)
+        return 0
+    urlPattern = ''  
+    
+    
+    
