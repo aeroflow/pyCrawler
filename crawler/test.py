dc169bea325327108cbb5bea43774cc26580a69f
user <user@user-PC>
2012-08-21 21:12:51 +0800

    test0.1

--- crawler/test.py
+++ crawler/test.py
@@ -2,24 +2,27 @@ import httplib2
 import re
 from html.parser import HTMLParser
 
-rootURL = 'http://www.dianping.com/search/category/2/0'
+rootURL = 'http://www.dianping.com/search/category/2/0/p1'
 
 urlPattern = re.compile(r'<a href="/shop/\d+"(.*)&nbsp;&nbsp;\d{8}</li>')
 
-def getRestaurant(url,filename):
+def getRestaurant(pattern,url):
     print(url)
-    print(filename)
+#    print(filename)
     try:
         response, content = httplib2.Http().request(url)
     except:
         print('download exception:' + response)
         return 0
-    f = open(filename, 'w')
+#    f = open(filename, 'w')
+    
+    results = pattern.findall(content)
+    print(results)
 '''    
 Parse the Html and get the information about restaurant
 
    
-'''
+
     
 def getURL(url):
     try:
@@ -29,5 +32,7 @@ def getURL(url):
         return 0
     urlPattern = ''  
     
-    
+''' 
+if __name__ == '__main__':
+    getRestaurant(urlPattern,rootURL)       
     
